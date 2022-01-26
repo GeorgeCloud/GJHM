2f4d0a631e7d3cbc7a1ef5721ba8d60238ed749d
GeorgeCloud <net.george.sky@gmail.com>
2022-01-26 12:52:24 -0800

    scaffold app.py - 1 route

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'hello world'
+
+
+if __name__ == '__main__':
+    app.run(port=8001, debug=True)
