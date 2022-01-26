4ba686e10e7feeff0c96b3da3b593149599519d0
GeorgeCloud <net.george.sky@gmail.com>
2022-01-26 13:02:49 -0800

    create templates folder. Setup index templating

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'hello world'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
