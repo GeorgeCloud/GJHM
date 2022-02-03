16bd118cfa0f6b89f19042448e7c944488934641
GeorgeCloud <net.george.sky@gmail.com>
2022-02-02 18:19:02 -0800

    create routes for users

--- blueprints/users.py
+++ blueprints/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect
 from datetime import datetime
 from db import *
 
@@ -15,26 +15,28 @@ def new_user():
 @users_bp.route('/create', methods=['POST'])
 def create_user():
     user = {
-        'email':      'email@gmail.com',
-        'first_name': 'John',
-        'last_name':  'Doe',
-        'password':   'password',
+        'email':      request.form['email'],
+        'first_name': request.form['first_name'],
+        'last_name':  request.form['last_name'],
+        'password':   request.form['password'],
         'avatar_url': '',
         'created_on': datetime.now(),
     }
 
-    user_id = db.users.insert_one(user).inserted_id
+    user_id = users.insert_one(user).inserted_id
 
-    return render_template('users_show.html', user_id=user_id)
+    return redirect(url_for('users_bp.view_user', user_id=user_id))
 
-@users_bp.route('/edit', methods=['GET'])
-def edit_user():
+@users_bp.route('<user_id>/edit', methods=['GET'])
+def edit_user(user_id):
     return 'edit page'
 
-@users_bp.route('/update', methods=['POST'])
+@users_bp.route('<user_id>/update', methods=['POST'])
 def update_user():
     return 'update page'
 
 @users_bp.route('/<user_id>', methods=['GET'])
 def view_user(user_id):
-    return f'user_id: {user_id}'
+    user = users.find_one({'_id': user_id})
+
+    return render_template('users_show.html', user=user)
