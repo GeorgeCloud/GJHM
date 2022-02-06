f2be70a6d34e95a959be22c7a3a7c19041b90cf2
GeorgeCloud <net.george.sky@gmail.com>
2022-02-06 13:32:14 -0800

    create delete user route

--- blueprints/users.py
+++ blueprints/users.py
@@ -6,7 +6,9 @@ users_bp = Blueprint('users_bp', __name__, template_folder='templates')
 
 @users_bp.route('/', methods=['GET'])
 def index_users():
-    return render_template('users.html')
+    all_users = users.find({})
+
+    return render_template('users.html', users=all_users)
 
 @users_bp.route('/new', methods=['GET'])
 def new_user():
@@ -16,27 +18,32 @@ def new_user():
 def create_user():
     user = {
         'email':      request.form['email'],
-        'first_name': request.form['first_name'],
-        'last_name':  request.form['last_name'],
+        'username':   request.form['username'],
+        'full_name': request.form['full_name'],
         'password':   request.form['password'],
         'avatar_url': '',
         'created_on': datetime.now(),
     }
 
-    user_id = users.insert_one(user).inserted_id
+    users.insert_one(user)
 
-    return redirect(url_for('users_bp.view_user', user_id=user_id))
+    return redirect(url_for('users_bp.view_user', username=user['username']))
 
-@users_bp.route('<user_id>/edit', methods=['GET'])
-def edit_user(user_id):
-    return 'edit page'
+@users_bp.route('/<username>', methods=['GET'])
+def view_user(username):
+    user = users.find_one({'username': username})
+    if user:
+        return render_template('users_show.html', user=user)
 
-@users_bp.route('<user_id>/update', methods=['POST'])
-def update_user():
-    return 'update page'
+    return 'User not found'
 
-@users_bp.route('/<user_id>', methods=['GET'])
-def view_user(user_id):
-    user = users.find_one({'_id': user_id})
+@users_bp.route('<username>/edit', methods=['GET', 'POST'])
+def edit_user(username):
+    """ Edit and Update Route """
+    pass
 
-    return render_template('users_show.html', user=user)
+@users_bp.route('<username>/delete', methods=['POST'])
+def delete_user(username):
+    users.find_one_and_delete({'username': username})
+
+    return 'user deleted'
