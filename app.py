179a7c74c172b2f344b0aed80b973953c801a7ee
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-02-16 13:01:26 -0800

    adding queries to display media on homepage

--- app.py
+++ app.py
@@ -14,9 +14,13 @@ api_search = tmdb.Search()
 
 @app.route('/', methods=['GET'])
 def homepage():
-    popular_media = tmdb.Movies().popular()['results']
-    print(popular_media)
-    return render_template('index.html')
+    trending_media = tmdb.Trending(media_type='movie', time_window='day')
+    # print(trending_media.info()['results'])
+    popular_movies = tmdb.Discover().movie(with_region='us', language='en', page=1)
+    popular_tvshows = tmdb.Discover().tv(with_region='us', language='en', page=1)
+
+    print(popular_tvshows['results'])
+    return render_template('index.html', trending_media=trending_media.info()['results'], popular_movies=popular_movies['results'], popular_tvshows=popular_tvshows['results'])
 
 
 if __name__ == '__main__':
